7ca5f8d3191dcc3d9c75462cfb02b2762b6dac3e
cybersecuritydef <155002701+cybersecuritydef@users.noreply.github.com>
2026-02-26 16:46:17 +0300

    Update filter.h

--- filter.h
+++ filter.h
@@ -12,27 +12,29 @@ public:
     Filter(){}
     ~Filter(){}
 
-    void words_length(std::list<std::string> &words, const size_t len_min=0, const size_t len_max=MAXLENPASS);
+    void wordsLength(std::list<std::string> &words, const size_t len_min=0, const size_t len_max=MAXLENPASS);
 
-    void remove_space(std::list<std::string> &words);
+    void removeSpace(std::list<std::string> &words);
 
-    void remove_digits(std::list<std::string> &words);
+    void removeDigits(std::list<std::string> &words);
 
     void digits(std::list<std::string> &words);
 
-    void remove_alpha(std::list<std::string> &words);
+    void removeAlpha(std::list<std::string> &words);
 
     void alpha(std::list<std::string> &words);
 
-    void remove_punct(std::list<std::string> &words);
+    void removePunct(std::list<std::string> &words);
+
+    void removeOther(std::list<std::string> &words);
 
     void punct(std::list<std::string> &words);
 
 private:
-    bool is_digits(const std::string &str);
+    bool isDigits(const std::string &str);
 
-    bool is_alpha(const std::string &str);
+    bool isAlpha(const std::string &str);
 
-    bool is_punct(const std::string &str);
+    bool isPunct(const std::string &str);
 };
 #endif // FILTER_H
